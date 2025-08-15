a522f10d46707c3f6bb359f27f387293aa7c65bf
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-08-16 00:21:48 +0530

    pages

---
+++ glia-app/src/components/FullSchedule.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FullSchedule() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ glia-app/src/components/Header.js
@@ -0,0 +1,28 @@
+// src/components/Header.js
+
+import React from 'react';
+
+const Header = ({ currentPage, setCurrentPage }) => {
+  return (
+    <header className="app-header">
+      <h1>GLIA</h1>
+      <p>XLIII Annual Meeting of Indian Academy of Neurosciences</p>
+      <nav className="main-nav">
+          <button 
+            onClick={() => setCurrentPage('schedule')}
+            className={currentPage === 'schedule' ? 'active' : ''}
+          >
+            Schedule
+          </button>
+          <button 
+            onClick={() => setCurrentPage('admin')}
+            className={currentPage === 'admin' ? 'active' : ''}
+          >
+            Admin
+          </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
