3ac6c39e4fd754d2ecb7211beeb63e8c8ab28cde
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-08-16 00:27:10 +0530

    Create Home.js

---
+++ glia-app/src/pages/Home.js
@@ -0,0 +1,16 @@
+// src/pages/HomePage.js
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <div className="welcome-text">
+        <p>Welcome to Advee</p>
+        <h1>What are you looking for?</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
