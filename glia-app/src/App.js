fc4c70669e317723663ef353436cc57781354b67
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-08-15 18:33:49 +0530

    sensor

--- glia-app/src/App.js
+++ glia-app/src/App.js
@@ -14,12 +14,12 @@ import {
 } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBc7mEMvFCS0ugnhmKZaxt1XP0Haf3spko",
-  authDomain: "glia-app-a6d84.firebaseapp.com",
-  projectId: "glia-app-a6d84",
-  storageBucket: "glia-app-a6d84.firebasestorage.app",
-  messagingSenderId: "228317645715",
-  appId: "1:228317645715:web:a0e13287c76621ff27b666"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 
