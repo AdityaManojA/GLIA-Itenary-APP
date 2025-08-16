28d21dd8adb1684a01b745548c27506fa606cfc2
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-08-16 23:44:49 +0530

    Update config.js

--- glia-app/src/firebase/config.js
+++ glia-app/src/firebase/config.js
@@ -12,7 +12,9 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+
 const app = initializeApp(firebaseConfig);
+
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
