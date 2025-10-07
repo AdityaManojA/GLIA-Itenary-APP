aed827a4abcbcfea4f4e17cdfe26184d393a46ba
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-10-07 13:54:53 +0530

    MAP UPDATES

--- glia-app/src/pages/MapPage.js
+++ glia-app/src/pages/MapPage.js
@@ -1,21 +1,22 @@
 import React from 'react';
-
-// The map links are now stored directly in the code
 const maps = [
+
   {
-    title: "Venue Map 1",
-    // This is a custom "My Maps" URL
-    url: "https://www.google.com/maps/d/embed?mid=195fYKK-5nyiBckRT4LGH80eqdt2a-rY"
-  },
-  {
-    title: "Venue Map 2",
-    // This is a standard Google Maps URL converted to an embeddable format
+    title: "CONFERENCE VENUE",
+   
     url: "https://maps.google.com/maps?q=8.402355,76.973196&z=15&output=embed"
   },
   {
-    title: "Venue Map 3",
+    title: "IAN-2025 BUS SHUTTLE ROUTES",
+   
+    url: "https://www.google.com/maps/d/embed?mid=195fYKK-5nyiBckRT4LGH80eqdt2a-rY"
+  },
+   {
+    title: "PRE-CONFERENCE WORKSHOP VENUE",
     url: "https://maps.google.com/maps?q=8.525263,76.907859&z=15&output=embed"
-  }
+  },
+  
+ 
 ];
 
 const MapPage = () => {
