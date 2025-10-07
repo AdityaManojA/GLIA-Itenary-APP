3e422981963a8359de3a49862d3c1db682373351
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-10-07 14:01:54 +0530

    Update MapPage.js

--- glia-app/src/pages/MapPage.js
+++ glia-app/src/pages/MapPage.js
@@ -1,22 +1,19 @@
 import React from 'react';
-const maps = [
 
+// The map links are now stored directly in the code with updated titles and order
+const maps = [
   {
-    title: "CONFERENCE VENUE",
-   
-    url: "https://maps.google.com/maps?q=8.402355,76.973196&z=15&output=embed"
-  },
-  {
-    title: "IAN-2025 BUS SHUTTLE ROUTES",
-   
-    url: "https://www.google.com/maps/d/embed?mid=195fYKK-5nyiBckRT4LGH80eqdt2a-rY"
-  },
-   {
     title: "PRE-CONFERENCE WORKSHOP VENUE",
     url: "https://maps.google.com/maps?q=8.525263,76.907859&z=15&output=embed"
   },
-  
- 
+  {
+    title: "IAN-2025 BUS SHUTTLE ROUTES",
+    url: "https://www.google.com/maps/d/embed?mid=195fYKK-5nyiBckRT4LGH80eqdt2a-rY"
+  },
+  {
+    title: "CONFERENCE VENUE",
+    url: "https://maps.google.com/maps?q=8.402355,76.973196&z=15&output=embed"
+  }
 ];
 
 const MapPage = () => {
